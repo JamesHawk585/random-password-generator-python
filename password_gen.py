68cc33aae41e7d1f07386c8604e787815f911a68
James Hawk <jph94880@gmail.com>
2025-03-23 15:46:36 -0500

    Adds prettycli and TerminalMenu

--- password_gen.py
+++ password_gen.py
@@ -3,7 +3,7 @@ from simple_term_menu import TerminalMenu
 import os
 import random 
 
-class cli():
+class Cli():
     def start(self): 
         # os.system("clear")
         print('Random Password Generaor')
@@ -12,27 +12,29 @@ class cli():
         menu_entry_index = terminal_menu.show()
         selection = options[menu_entry_index]
         return selection
-    def generate_random_password():
-        password_length = 10
-        lowercase_chars = 'abcdefghijklmnopqrstuvwxyz'
-        uppercase_chars = lowercase_chars.upper()
-        numbers = '0123456789'
-        special_chars = '/!@#$%^&*()-_=}+[]{`|;:/,.<>?/\"'
-        password=''
-        my_string = lowercase_chars + uppercase_chars + numbers + special_chars
-        for x in range(password_length):
-            random_char = random.randrange(1, 93)
-            password += my_string[random_char]
-        print(password)
-        return password
+    
+def generate_random_password(selection):
+    password_length = 10
+    lowercase_chars = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase_chars = lowercase_chars.upper()
+    numbers = '0123456789'
+    special_chars = '/!@#$%^&*()-_=}+[]{`|;:/,.<>?/\"'
+    password=''
+    my_string = lowercase_chars + uppercase_chars + numbers + special_chars
+    for x in range(password_length):
+        random_char = random.randrange(1, 93)
+        password += my_string[random_char]
+    print(password)
+    return password
+
+def handle_selection(selection): 
+    generate_random_password(selection)
 
         # print(my_string[random_char])
 
-
-        pass
-
-
-    generate_random_password()
+app = Cli()
+selection = app.start()
+handle_selection(selection)
 
 
     
